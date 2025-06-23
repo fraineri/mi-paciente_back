d39e56ec184a2a0076a05e93eaa12eb679d3a049
Franco Raineri <fraineri@outlook.com>
2025-06-22 23:28:10 -0300

    feat: Update health endpoints to include specific paths and improve function naming

--- src/entry_points/health.py
+++ src/entry_points/health.py
@@ -1,5 +1,5 @@
 from dependency_injector.wiring import inject
-from fastapi import status
+from fastapi import Body, status
 
 from core.route_management.decorators import route
 from core.route_management.enums import RouteVisibility
@@ -8,8 +8,15 @@ from core.route_management.router import VersionedAPIRouter, create_versioned_ro
 router: VersionedAPIRouter = create_versioned_router(path="/health")
 
 
-@router.get(path="", status_code=status.HTTP_200_OK)
+@router.get(path="/get", status_code=status.HTTP_200_OK)
 @route(visibility=RouteVisibility.PUBLIC, version=(1, 0))
 @inject
-async def ping():
+async def get_ping():
     return {"status": "ok"}
+
+
+@router.post(path="/post", status_code=status.HTTP_200_OK)
+@route(visibility=RouteVisibility.PUBLIC, version=(1, 0))
+@inject
+async def post_ping(body: dict = Body(...)):  # noqa: B008
+    return {"status": "ok", "method": "post", "body": body}
